69bff47b5b3e05c98a7e8c26ad41a4af943dcf3a
mumbahiz <liqoo@nostratech.com>
2024-05-26 01:32:29 +0700

    Add main.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./dist/css/main.css";
+import "./style/css/main.css";
 
 import { BrowserRouter } from "react-router-dom";
 
